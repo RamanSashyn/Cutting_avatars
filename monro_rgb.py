2fd44d60826b0a4fdf88b88e9cbcbe361ecb0a0d
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-11-22 16:41:16 +0100

    Assemble a picture from the channels

--- monro_rgb.py
+++ monro_rgb.py
@@ -1,4 +1,5 @@
 from PIL import Image
+import os
 
 image = Image.open('monro.jpg')
 print(image.mode)
@@ -6,4 +7,7 @@ red_channel, green_channel, blue_channel = image.split()
 red_channel.save('red_channel.jpg')
 green_channel.save('green_channel.jpg')
 blue_channel.save('blue_channel.jpg')
+os.remove('monro.jpg')
+new_image = Image.merge('RGB', (red_channel, green_channel, blue_channel))
+new_image.save('monro.jpg')
 
