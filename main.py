65e4194669975bc89338f56a3388aad4d3906de6
Raman <149716575+RamanSashyn@users.noreply.github.com>
2024-11-22 19:36:39 +0100

    Отправка файлов

--- main.py
+++
@@ -1,8 +0,0 @@
-from PIL import Image
-
-image = Image.open('example.jpg')
-rotated_image = image.rotate(45)
-rotated_image.save('rotated.jpg')
-print("Ширина -", image.width)
-print("Высота -", image.height)
-print("Цветовая модель -", image.mode)
